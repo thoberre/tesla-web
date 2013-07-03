bc2f3c7b0d705fe095c4c287c7a7047b98bc1c97
Thom Mikail S. Berre <thom@berreinvest.no>
2013-07-03 10:51:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeslaWeb::Application.config.secret_key_base = 'b9268ae3bfddd3a8c2bc73acde9a92f39ad381f5f7509086e77ad8ffd09a5943ac100c6a891415e98e77512540c2e326f0802caf986e9385e015af0c0fecbb7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeslaWeb::Application.config.session_store :cookie_store, key: '_tesla-web_session'
