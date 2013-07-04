d201c2977597b4cbeb520a23f94d0ebb4185289d
Thom Mikail S. Berre <thom@berreinvest.no>
2013-07-04 11:34:19 +0200

    Config change for ssl based on environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ TeslaWeb::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # SSL
+  config.force_ssl = false
 end
